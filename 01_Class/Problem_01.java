00ee062ce73035cf9f3632e61bd7689a67cd9329
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-25 22:36:48 +0530

    class objects changes

---
+++ 01_Class/Problem_01.java
@@ -0,0 +1,34 @@
+class Account{
+    public String accountHolderName;
+    public String accountNumber;
+    public double accountBalance;
+
+    public static void printResult(){
+        System.out.println("Account Holder Name: "+ a1.accountHolderName);
+        System.out.println("Account Number: "+ a1.accountNumber);
+        System.out.println("Account Balance: "+ a1.accountBalance);
+    }
+
+    public static void Account(String a, String b, double c){
+        this.accountHolderName = a;
+        this.accountNumber = b;
+        this.accountBalance = c;
+    }
+}
+
+public class Problem_01{
+    public static void main(String[] args) {
+        Account a1 = new Account();
+
+        a1.accountHolderName = "Shivanand kumar";
+        a1.accountNumber = "39934854059";
+        a1.accountBalance = 455467;
+
+        a1.printResult();
+
+        // System.out.println("Account Holder Name: "+ a1.accountHolderName);
+        // System.out.println("Account Number: "+ a1.accountNumber);
+        // System.out.println("Account Balance: "+ a1.accountBalance);
+
+    }
+}
